6372c2b6979265d9f303af4d9165df765d9328f0
RConradGe <rcgadduang@gmail.com>
2025-04-29 09:39:33 -0700

    wip: updated settings, testing url page

--- iCollect/iCollect/settings.py
+++ iCollect/iCollect/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7y6z^i#-*8p*h-!@bw4#)_wtphob^ueo7rw&tkdjeixmp$q1h@'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
