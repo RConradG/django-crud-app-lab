4d34d5edfc097a0c08f86b8c0010b04152906de3
RConradGe <rcgadduang@gmail.com>
2025-04-30 09:53:47 -0700

    wip: added Category, Collection, and item Models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length = 100)
+    
+    def __str__(self):
+        return self.name
+
+class Collection(models.Model):
+    description = models.TextField(blank=True)
+    
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    name = models.Charfield(max_length=100)
+    description = models.TextField(blank=True)
+    purchase_date = models.DateField(null=True, blank=True)
+    estimated_value = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return self.name
+    
